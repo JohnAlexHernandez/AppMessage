51cb4c1ce3a12012b80941de0de6dc26ecb59e35
JohnAlexHernandez <90jalexhernandez09@gmail.com>
2026-06-30 16:45:23 -0500

    Se ajustan las propiedad database en la conexion mysql

--- backend_message/src/config/db.js
+++ backend_message/src/config/db.js
@@ -3,10 +3,10 @@ const mysql = require('mysql2');
 // Creamos un pool de conexiones a la base de datos MySQL utilizando el módulo mysql2
 const pool = mysql.createPool({
   host: process.env.DB_HOST,
-  port: process.env.BD_PORT,
+  port: process.env.DB_PORT,
   user: process.env.DB_USER,
   password: process.env.DB_PASSWORD,
-  database: process.env.BD_NAME,
+  database: process.env.DB_NAME,
   waitForConnections: true,
   connectionLimit: 10,
   queueLimit: 0
